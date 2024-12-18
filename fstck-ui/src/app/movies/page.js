4a1b545f8182ff9727b12e62666866cd0724648c
Oleksii S <savran333@gmail.com>
2024-12-18 16:30:32 +0100

    feat(ui): enabled pagination

--- fstck-ui/src/app/movies/page.js
+++ fstck-ui/src/app/movies/page.js
@@ -1,9 +1,11 @@
+'use client';
+
 import MovieCard from "@/components/movieCard/movieCard";
 import Sidebar from "@/components/sidebar";
 import Pagination from "@/components/pagination";
-import {cache} from "react";
+import {useCallback, useEffect, useState} from "react";
 
-async function getMovies(page = 1, limit = 10) {
+async function getMovies(page = 1, limit = 20) {
   try {
     const response = await fetch(`http://localhost:3001/movie?page=${page}&limit=${limit}`);
     return await response.json();
@@ -13,9 +15,21 @@ async function getMovies(page = 1, limit = 10) {
   }
 }
 
-export default async function MovieListPage() {
-  const data = await getMovies();
+export default function MovieListPage() {
+  const [data, setData] = useState({ movies: [], metadata: {page: 1, limit: 20, totalPages:0}});
   const { page, limit, totalPages } = data.metadata;
+
+  const fetchMovies = useCallback((page = 1, limit = 20) => {
+    getMovies(page, limit).then((resp) => {
+      if (resp) {
+        setData(resp);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchMovies();
+  }, [fetchMovies]);
 
   return (
     <div>
@@ -23,7 +37,7 @@ export default async function MovieListPage() {
         <h1 className="text-3xl mb-6">Movies</h1>
         <Sidebar/>
       </div>
-      <Pagination page={page} totalPages={totalPages} limit={limit}/>
+      <Pagination page={page} totalPages={totalPages} limit={limit} onPageChange={fetchMovies}/>
       {data.movies.map((movie) => <MovieCard className="mb-4" key={movie._id} movie={movie} />)}
       <Pagination page={page} totalPages={totalPages} limit={limit}/>
 
--- fstck-ui/src/components/pagination.js
+++ fstck-ui/src/components/pagination.js
@@ -1,25 +1,19 @@
-'use client';
-
 import clsx from "clsx";
 
 export default function Pagination({ page, limit, totalPages, onPageChange }) {
   // const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
 
-  // const handlePageChange = async (newPage) => {
-  //   await onPageChange(newPage, limit); // Invoke the fetcher
-  // };
-
   return (
     <div className="flex justify-end items-center my-4">
       <button className={clsx("px-2 py-1 mx-1 border border-gray-300", {"bg-gray-700": page === 1})}
-              disabled={page === 1}>
+              disabled={page === 1} onClick={() => onPageChange(page - 1)}>
         Prev
       </button>
       <button className="px-2 py-1 mx-1 border border-gray-300 bg-gray-700" disabled>
         {page}
       </button>
-      <button className="px-2 py-1 mx-1 border border-gray-300">Next</button>
-      <button className="px-2 py-1 mx-1 border border-gray-300">Next 10</button>
+      <button className="px-2 py-1 mx-1 border border-gray-300" onClick={() => onPageChange(page + 1)}>Next</button>
+      <button className="px-2 py-1 mx-1 border border-gray-300" onClick={() => onPageChange(page + 10)}>Next 10</button>
     </div>
   );
 }
